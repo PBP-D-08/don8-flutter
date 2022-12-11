3206027a038650d837b4a857dcf54d5023a0905c
Mariana Salma <marianasalma11@gmail.com>
2022-12-11 16:48:03 +0700

    finish add new donation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:don8_flutter/new_donation/newdonation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'common/constants.dart';
@@ -32,6 +33,7 @@ class MyApp extends StatelessWidget {
             "/login": (BuildContext context) => const LoginPage(),
             "/register": (BuildContext context) => const RegisterPage(),
             "/example": (BuildContext context) => const ContohPenggunaan(),
+            "/newdonation": (BuildContext context) => const NewDonation(),
           },
         ));
   }
